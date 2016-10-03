eacbc2d7aaa806b34456014449312b845ea28e88
ueliton <freitas.ueliton@gmail.com>
2016-10-02 20:50:58 -0400

    Impelemtando prieira activity

---
+++ WeatherApp/app/src/main/java/com/brasileirao/weatherapp/ForecastListAdapter.kt
@@ -0,0 +1,26 @@
+package com.brasileirao.weatherapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by ueliton on 10/2/16.
+ */
+
+class ForecastListAdapter(val items : List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent?.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.textView?.text = this.items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(val textView : TextView) : RecyclerView.ViewHolder(textView)
+}
